c6e4a157c47e9e72134a3ed2bf891ee84d8f14e4
0grazi00 <128594313+0grazi00@users.noreply.github.com>
2024-09-19 08:14:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+export const perguntas = [
+    {
+        enunciado: "Você acredita que saber a quantidade exata de calorias em sua refeição pode ajudar a atingir seus objetivos de saúde?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Sim, saber as calorias dos alimentos pode ser muito útil para atingir metas de saúde e bem-estar. Conhecer o valor calórico dos alimentos ajuda a controlar a ingestão total, facilitando o equilíbrio entre o que se come e as necessidades energéticas do corpo. Isso é especialmente importante para quem busca perder peso, ganhar massa muscular ou manter um estilo de vida saudável. Além disso, entender as calorias pode incentivar escolhas alimentares mais conscientes e nutritivas!"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Sim, saber as calorias dos alimentos pode ser muito útil para atingir metas de saúde e bem-estar. Conhecer o valor calórico dos alimentos ajuda a controlar a ingestão total, facilitando o equilíbrio entre o que se come e as necessidades energéticas do corpo. Isso é especialmente importante para quem busca perder peso, ganhar massa muscular ou manter um estilo de vida saudável. Além disso, entender as calorias pode incentivar escolhas alimentares mais conscientes e nutritivas!"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "É possível equilibrar uma dieta saudável apenas contando carboidratos, sem considerar proteínas e gorduras?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Proteínas e gorduras na dieta são importantes porque desempenham papéis essenciais no funcionamento do corpo, assim, uma dieta equilibrada que inclua adequadamente proteínas e gorduras é vital para a saúde geral e o bem-estar."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Proteínas e gorduras na dieta são importantes porque desempenham papéis essenciais no funcionamento do corpo, assim, uma dieta equilibrada que inclua adequadamente proteínas e gorduras é vital para a saúde geral e o bem-estar."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você acha que os cálculos nutricionais podem tornar a alimentação mais divertida e interessante?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Sim, os cálculos nutricionais ajudam a tornar o processo de alimentação saudável mais fácil. Ao fornecer informações precisas sobre a ingestão de calorias, macronutrientes e micronutrientes, eles permitem que as pessoas tomem decisões alimentares mais informadas. Isso facilita o planejamento de refeições, a escolha de alimentos e o acompanhamento de metas nutricionais. Com essa abordagem, é possível equilibrar a dieta de maneira mais eficiente, promovendo uma alimentação saudável e adaptada às necessidades individuais."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Sim, os cálculos nutricionais ajudam a tornar o processo de alimentação saudável mais fácil. Ao fornecer informações precisas sobre a ingestão de calorias, macronutrientes e micronutrientes, eles permitem que as pessoas tomem decisões alimentares mais informadas. Isso facilita o planejamento de refeições, a escolha de alimentos e o acompanhamento de metas nutricionais. Com essa abordagem, é possível equilibrar a dieta de maneira mais eficiente, promovendo uma alimentação saudável e adaptada às necessidades individuais."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+];
